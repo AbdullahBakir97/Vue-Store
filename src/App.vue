106fee398702c8be7860e80f5db83b521380fb68
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-02-04 20:57:49 +0100

    delete

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -8,7 +7,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
